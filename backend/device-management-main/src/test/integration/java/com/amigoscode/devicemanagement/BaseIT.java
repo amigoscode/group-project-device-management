6c60824d17a1e15eabcdd86c3af7d435e342b1d1
rafal-nowak <contact@rafal-nowak.com>
2023-03-11 09:46:06 +0100

    refactored BaseIT

--- backend/device-management-main/src/test/integration/java/com/amigoscode/devicemanagement/BaseIT.java
+++ backend/device-management-main/src/test/integration/java/com/amigoscode/devicemanagement/BaseIT.java
@@ -3,17 +3,26 @@ package com.amigoscode.devicemanagement;
 import com.amigoscode.devicemanagement.domain.user.UserService;
 import com.amigoscode.devicemanagement.domain.user.model.User;
 import com.amigoscode.devicemanagement.domain.user.model.UserRole;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -25,11 +34,14 @@ import java.util.Set;
 @ActiveProfiles("it")
 @AutoConfigureDataMongo
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-    classes = DeviceManagementApplication.class
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        classes = DeviceManagementApplication.class
 )
 @ExtendWith(SpringExtension.class)
 public class BaseIT {
+
+    @Autowired
+    protected Environment environment;
 
     @Autowired
     protected TestRestTemplate restTemplate;
@@ -42,13 +54,44 @@ public class BaseIT {
     @Autowired
     MongoTemplate mongoTemplate;
 
+    @Autowired
+    protected MqttPahoClientFactory mqttPahoClientFactory;
+
+    protected IMqttClient client;
+
     @BeforeEach
     void init() {
+        setUpMqttConnection();
         dropDb();
         addTestUsers();
     }
 
-    protected void dropDb(){
+    @AfterEach
+    void deinit() {
+        tearDownMqttConnection();
+    }
+
+    @SneakyThrows
+    public void setUpMqttConnection() {
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setServerURIs(new String[]{environment.getProperty("mqtt.server.uri")});
+        options.setUserName(environment.getProperty("mqtt.username"));
+        String pass = environment.getProperty("mqtt.password");
+        options.setPassword(pass.toCharArray());
+        options.setCleanSession(true);
+
+        client = new MqttClient(environment.getProperty("mqtt.server.uri"), "fakeMqttClientId");
+        client.connect(options);
+    }
+
+    @SneakyThrows
+    public void tearDownMqttConnection() {
+        client.disconnect();
+        client.close();
+    }
+
+    protected void dropDb() {
         mongoTemplate.getDb().drop();
     }
 
@@ -58,7 +101,7 @@ public class BaseIT {
             "Stefan Burczymucha",
             passwordEncoder.encode("password"),
             Set.of(UserRole.ADMIN)
-        );
+    );
 
     protected String localUrl(String endpoint) {
         return "http://localhost:7777" + endpoint;
@@ -72,9 +115,9 @@ public class BaseIT {
         String token = "Basic ";
         try {
             token = token + Base64.getEncoder()
-                .encodeToString(
-                    (email + ":" + password).getBytes("UTF-8")
-                );
+                    .encodeToString(
+                            (email + ":" + password).getBytes("UTF-8")
+                    );
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -85,13 +128,25 @@ public class BaseIT {
         String adminToken = "Basic ";
         try {
             adminToken = adminToken + Base64.getEncoder()
-                .encodeToString(
-                    (adminUser.getEmail() + ":" + adminUser.getPassword()).getBytes("UTF-8")
-                );
+                    .encodeToString(
+                            (adminUser.getEmail() + ":" + adminUser.getPassword()).getBytes("UTF-8")
+                    );
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         return adminToken;
+    }
+
+    @SneakyThrows
+    protected <T> void publishMqttMessage(
+            String mqttTopic,
+            T payload) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        String measurementJsonInString = mapper.writeValueAsString(payload);
+        MqttMessage mqttMessage = new MqttMessage();
+        mqttMessage.setPayload(measurementJsonInString.getBytes());
+        client.publish(mqttTopic, mqttMessage);
     }
 
     protected <T, U> ResponseEntity<U> callHttpMethod(
@@ -112,10 +167,10 @@ public class BaseIT {
             requestEntity = new HttpEntity<>(body, headers);
         }
         return restTemplate.exchange(
-            localUrl(url),
-            httpMethod,
-            requestEntity,
-            mapToObject
+                localUrl(url),
+                httpMethod,
+                requestEntity,
+                mapToObject
         );
     }
 
